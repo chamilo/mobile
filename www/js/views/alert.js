5d14d4b6c99e5cff1e3be76fb69118eb37f916b3
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2015-07-30 02:14:59 -0500

    Show alert messages like Bootstrap's tooltip

--- www/js/views/alert.js
+++ www/js/views/alert.js
@@ -4,11 +4,21 @@ define([
     'text!template/alert.html'
 ], function (_, Backbone, AlertTemplate) {
     var AlertView = Backbone.View.extend({
+        initialize: function () {
+            this.render();
+        },
+        className: 'alert-container',
         template: _.template(AlertTemplate),
         render: function () {
+            var self = this;
+
             this.el.innerHTML = this.template(this.model);
 
-            return this;
+            document.body.appendChild(self.el);
+
+            window.setTimeout(function () {
+                document.body.removeChild(self.el);
+            }, 3000);
         }
     });
 
